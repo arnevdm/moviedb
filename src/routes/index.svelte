81373faea3df536c988f32e85b0906b03539a46a
arnevdm <arnevanimus@gmail.com>
2022-05-14 01:01:53 +0200

    third commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,26 @@
-<h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur eius exercitationem veniam voluptate quae repellendus, quis a neque aut iste consequatur rerum, quo earum est blanditiis! Asperiores quasi ea necessitatibus.</h1>
+<script context="module">
+  export async function load({fetch}){
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API}&language=en-US&page=1`);
+    const data = await res.json();
+    console.log(data);
+      if(res.ok){
+        return{
+          props:{ popular: data.results }
+    };
+  }
+}
+</script>
+
+<script>
+
+  import PopularMovies from '../components/PopularMovies.svelte';
+  import SearchMovies from '../components/SearchMovies.svelte';
+  export let popular;
+  import { fly } from 'svelte/transition'
+</script>
+
+
+<section transition:fly="{{ duration: 500, delay: 500}}" >
+  <SearchMovies />
+  <PopularMovies {popular}/>
+</section>
---
+++ src/routes/movie/[id].svelte
@@ -0,0 +1,63 @@
+<script context="module">
+  export async function load({fetch, params}){
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${import.meta.env.VITE_API}&language=en`);
+    const movieDetail = await res.json();
+      if(res.ok){
+        return{
+          props:{ movieDetail}
+    };
+  }
+}
+</script>
+
+<script>
+  export let movieDetail;
+  import { fly } from 'svelte/transition';
+</script>
+
+<div class="movie-details" transition:fly="{{ duration: 500, delay: 500}}">
+  <div class="img-container">
+    <img src={'https://image.tmdb.org/t/p/original' + movieDetail.backdrop_path}
+    alt={movieDetail.title}>
+  </div>
+  <div class="text-container">
+    <h1>{movieDetail.title}</h1>
+    <p class="overview">{movieDetail.overview}</p>
+    <p>
+      <span>Release date:</span>
+      {movieDetail.release_date} <br />
+      <span>Budget:</span> ${movieDetail.budget} <br />
+      <span>Rating:</span>
+      {movieDetail.vote_average} <br />
+      <span>Runtime:</span>
+      {movieDetail.runtime}mins
+    </p>
+  </div>
+</div>
+
+<style>
+  h1 {
+    padding: 1rem 0rem 2rem;
+  }
+
+  p {
+    padding: 1rem 0rem;
+  }
+
+  .img-container {
+    width:100%;
+  }
+
+  img {
+    width: 100%;
+    border-radius: 1rem;
+  }
+
+  .movie-details {
+    margin: 2rem 20%;
+  }
+
+  span {
+    font-weight: bold;
+  }
+</style>
